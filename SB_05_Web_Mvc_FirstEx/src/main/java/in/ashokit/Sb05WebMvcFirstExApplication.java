5b37e5f03d35deaaf5132623e24d2f2af76c359b
Raghab-13 <raghabendra13@gmail.com>
2024-12-08 19:12:40 +0530

    first web mvc example

---
+++ SB_05_Web_Mvc_FirstEx/src/main/java/in/ashokit/Sb05WebMvcFirstExApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb05WebMvcFirstExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb05WebMvcFirstExApplication.class, args);
+	}
+
+}
